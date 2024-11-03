b8c62f2e1d8ab18599bb3a5d07e4a272c15e59d8
Mary Ng <mn1520@nyu.edu>
2024-11-02 21:38:53 -0400

    removed utils.py

--- app/utils.py
+++
@@ -1,9 +0,0 @@
-import requests
-from PIL import Image
-
-
-def save_image(image):
-    image = request.files['image'] # Get the image from the request
-    filename = secure_filename(image.filename) # Sanitize the filename
-    image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename) # Define the path to save
-    image.save(image_path) # Save the image
